f12e6acbb36e2452a5fb724cf11b4f0ba1caad4f
Rob Stemen <rstemen@bigpanda.io>
2024-01-10 10:36:23 -0600

    Update subscription URL for OIM integrations
    Addresses [BP-14464]

--- sns.tf
+++ sns.tf
@@ -5,5 +5,5 @@ resource "aws_sns_topic" "sns_topic" {
 resource "aws_sns_topic_subscription" "sns_subscription" {
   topic_arn = aws_sns_topic.sns_topic.arn
   protocol  = "https"
-  endpoint  = "${var.api_endpoint}/cloudwatch/alerts?access_token=${var.bearer_token}&app_key=${var.app_key}"
+  endpoint  = "${var.api_endpoint}/cloudwatch_v2/alerts?access_token=${var.bearer_token}&app_key=${var.app_key}"
 }
